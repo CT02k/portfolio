e15c7adc127a81aa69e903fe8e6e415d8a3d3690
CT02k <joaopaulocfwork@gmail.com>
2025-11-07 22:41:56 -0300

    refactor(projects-section): removido as váriaveis hard coded e agora usando o endpoint /projects

--- apps/web/app/components/sections/Projects.tsx
+++ apps/web/app/components/sections/Projects.tsx
@@ -1,29 +1,9 @@
 import CT02 from "@/app/svgs/ct02";
 import ProjectCard from "../ui/ProjectCard";
+import { apiRequest, ProjectsResponse } from "@/app/lib/api";
 
-const projects = [
-  {
-    title: "Portfolio",
-    description: "Meu portfolio, o site que você está agora.",
-    image: "/projetos/ct02.png",
-    tags: ["Design", "Programação"],
-  },
-  {
-    title: "Flow Finance",
-    description: "Aplicação de gestão financeira pessoal inteligente.",
-    image: "/projetos/flow-finance.png",
-    tags: ["Design", "Programação"],
-  },
-  {
-    title: "Goanime",
-    description:
-      "Design da logo, icone e tipografia da Goanime, um player de anime para terminal feito em Golang.",
-    image: "/projetos/go-anime.png",
-    tags: ["Design"],
-  },
-];
-
-export default function Projects() {
+export default async function Projects() {
+  const { projects } = await apiRequest<ProjectsResponse>("/projects");
   return (
     <section
       id="projects"
